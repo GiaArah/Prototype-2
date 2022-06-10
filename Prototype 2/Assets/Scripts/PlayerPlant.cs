845b5915ecf2c5b7b1a176a224c8dfafbf0cd81b
Soruya <soruyamageofmemories@gmail.com>
2022-06-10 16:33:16 -0700

    Got rudimentary plant growing code

--- Prototype 2/Assets/Scripts/PlayerPlant.cs
+++ Prototype 2/Assets/Scripts/PlayerPlant.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class PlayerPlant : MonoBehaviour
 {
     public GameObject plant;
+    public Transform plantDimensions;
+    public Vector3 plantScale;
 
     // Start is called before the first frame update
     void Start()
     {
         plant = GameObject.Find("Plant");
+        plantDimensions = GameObject.Find("Plant").transform;
+        plantScale = Vector3.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Line for testing
+        //GrowOrWilt(0.5f, plantScale);
     }
+
+    //Vector3 direction should be positive for growth, negative for wilt
+    public void GrowOrWilt(float amount, Vector3 direction)
+    {
+        plantDimensions.localScale += direction * amount;
+    }
+
 }
