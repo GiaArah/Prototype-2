21acad944419208f16d2eb075c5b1dfd9e35797b
Soruya <soruyamageofmemories@gmail.com>
2022-06-10 22:31:01 -0700

    Plant auto grows

--- Prototype 2/Assets/Scripts/PlayerPlant.cs
+++ Prototype 2/Assets/Scripts/PlayerPlant.cs
@@ -9,6 +9,10 @@ public class PlayerPlant : MonoBehaviour
     public Vector3 plantGrow;
     public Vector3 plantWilt;
 
+    Vector3 plantRisePos;
+
+    public GameObject plantPiecePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,19 +20,30 @@ public class PlayerPlant : MonoBehaviour
         plantDimensions = GameObject.Find("Plant").transform;
         plantGrow = Vector3.up;
         plantWilt = Vector3.down;
+
+        plantRisePos = plant.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Line for testing
-        GrowOrWilt(0.5f, plantGrow);
+        //GrowOrWilt(0.5f, plantGrow);
+        GrowSprite();
     }
 
     //Vector3 direction should be positive for growth, negative for wilt
+    //Downside: The sprite will scale both ways anyway
     public void GrowOrWilt(float amount, Vector3 direction)
     {
         plantDimensions.localScale += direction * amount;
+    }
+
+    public void GrowSprite()
+    {
+        GameObject plantPiece = (GameObject)Instantiate(plantPiecePrefab, plantRisePos + new Vector3(0.0f, 6.0f, 0.0f), Quaternion.identity);
+
+        plantRisePos = plantPiece.transform.position;
     }
 
 }
